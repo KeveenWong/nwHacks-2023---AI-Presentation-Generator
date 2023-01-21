15574c2b12b59e9d8a76e3f3183668be00391ef8
Kol Crooks <kolcrooks10@gmail.com>
2023-01-21 14:00:36 -0800

    added auth

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -9,5 +9,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if(req.method !== 'POST') return res.status(405).end()
+  req.body
 }
