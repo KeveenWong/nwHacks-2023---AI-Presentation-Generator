50fe4d45c85c44eb612bfe2c6102f772f2de72d1
Kol Crooks <kolcrooks10@gmail.com>
2023-01-21 19:03:35 -0800

    google sheets stuff

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,14 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { slides_v1 } from 'googleapis';
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {createPresentation, createTwoColumnSlide} from '../../util/googleslides';
 
-type Data = {
-  name: string
+type RequestData = {
+  
 }
 
-export default function handler(
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  if(req.method !== 'POST') return res.status(405).end()
-  req.body
+  if(req.method !== 'POST') return res.status(405).end();
+
+  const [link, id] = await createPresentation("TESTETSTTEST", [[
+    "TITLE", "HELLO TEST", "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+  ]]);
+  console.log(link);
+  // await createTwoColumnSlide(id || "", "HELLO TEST", "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+  // console.log(link);
+  return res.status(200).end();
 }
